5fe2ac907e5c70f30d69918cace24279b49c901c
Nicolai Stoianov <stoianovnk@gmail.com>
2019-10-10 18:38:41 +0300

    Set timezone to Kiev; Add mongoid to config

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,10 @@ module ChisinauAir
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Europe/Kiev'
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.orm :mongoid
+    end
   end
 end
