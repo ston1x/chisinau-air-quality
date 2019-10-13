d92f388197e546b747e91c9cd9b8c5238ddcbe6b
Nicolai Stoianov <stoianovnk@gmail.com>
2019-10-14 00:23:27 +0300

    Add thank you page & root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :reports, only: [:create, :index]
+  resources :reports, only: [:create, :index, :thanks]
+  get 'thank_you' => 'reports#thank_you'
+  root to: 'reports#index'
 end
