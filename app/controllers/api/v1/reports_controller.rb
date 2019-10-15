75471328d33d8e39c126e876d8081cc4eb6b4ec2
Nicolai Stoianov <stoianovnk@gmail.com>
2019-10-15 19:27:53 +0300

    Add API namespace & reports controller

---
+++ app/controllers/api/v1/reports_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::ReportsController < Api::V1::BaseController
+  def index
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   resources :reports, only: [:create, :index, :thanks]
   get 'thank_you' => 'reports#thank_you'
   root to: 'reports#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :reports, only: [:create, :index]
+    end
+  end
 end
